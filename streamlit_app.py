f731a45c862f5fb7b37b8c8391c3e96089569194
andre123354cs <edwar.vaco@gmail.com>
2024-12-05 09:09:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,8 @@ st.set_page_config(layout="wide")
 
 imagen_perfil = "https://media.licdn.com/dms/image/v2/D4E03AQEJOCsUNzF9rw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1712973716317?e=1738800000&v=beta&t=HPuQ9s25HudkUSBEHLYVsaBXbD39HazKqrLCTkwKEx4"
 
-
-st.title("Andrés Vanegas")
 st.image(imagen_perfil, width=250)
+st.title("Andrés Vanegas")
 st.subheader("Técnico en Sistemas | Administrador de Empresas")
 
 st.write("Soy Andrés Vanegas, un apasionado por la tecnología y la administración. Actualmente me encuentro cursando el quinto semestre de Administración de Empresas y soy técnico en sistemas con énfasis en desarrollo.")
