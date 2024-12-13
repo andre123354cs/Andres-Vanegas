e73fedb957da5dc8b51f199c1e1fbdf3088c0e68
andre123354cs <edwar.vaco@gmail.com>
2024-12-13 08:39:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,27 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="MetaData",
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
 
-imagen_perfil = "https://media.licdn.com/dms/image/v2/D4E03AQEJOCsUNzF9rw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1712973716317?e=1738800000&v=beta&t=HPuQ9s25HudkUSBEHLYVsaBXbD39HazKqrLCTkwKEx4"
+st.markdown("""
+  <div style="display: flex; justify-content: Center; align-items: Center;">
+    <img src="https://cdn-icons-png.flaticon.com/128/2118/2118460.png" alt="RRHH YesBpo Logo" width="100" height="100">
+    <h1 style='color: #0f0a68; font-size: 29px;'> Proyecto Recursos Humanos</h1>
+  </div>
+""", unsafe_allow_html=True)
 
-st.image(imagen_perfil, width=250)
-st.title("Andrés Vanegas")
-st.subheader("Técnico en Sistemas | Administrador de Empresas")
-st.write("Telefono | 3145594483")
-st.write("Mail | Edwar.vaco@gmail.com")
+st.markdown("""
+    <h1 style='text-align: left; color: #0f0a68; font-size: 15px;'>Transparencia y claridad en cada paso. Conoce el estado de tus solicitudes y mantente informado sobre los procesos de RRHH. ¡Tu tranquilidad es nuestra prioridad!</h1>
+    """, unsafe_allow_html=True)
 
-st.write("Coordinador de Inteligencia Empresarial. | Extracción de datos, Preprocesamiento, Modelado, Algoritmos, Aprendizaje automático, Clasificación, Agrupamiento, Análisis de patrones, Minería de texto Y Visualización de datos.")
-st.write("________________________________________________________________________________________________")
+gsheetid = '14NM6hoF3dg-Veq7w1OBZEwH0B1w9kq8mxt07-apSBIc'
+sheetod = '0'
+url = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid={sheetod}&format'
 
-st.markdown("Técnico altamente capacitado en el análisis de datos y la optimización de procesos, con una sólida experiencia en el sector aeronáutico y el transporte aéreo de carga. Con más de dos años de experiencia, he demostrado habilidades excepcionales en la recopilación, análisis e interpretación de datos para impulsar la toma de decisiones estratégicas y mejorar la eficiencia operativa. Recientemente, me he inclinado como coordinador BI sobre negocios de cartera, optimizando la gestión y análisis de portafolios para maximizar su rendimiento")
-
-# Habilidades
-st.header("Habilidades")
-st.write("""
-* **Desarrollo:** Python, JavaScript, HTML, CSS, React
-* **Bases de datos:** SQL, PostgreSQL
-* **Herramientas:** Git, Docker, AWS
-* **Blandas:** Trabajo en equipo, resolución de problemas, comunicación efectiva
-""")
-
-# Proyectos
-st.header("Proyectos")
-st.write("""
-* **Proyecto 1:** Breve descripción del proyecto 1, tecnologías utilizadas, resultados obtenidos.
-* **Proyecto 2:** Breve descripción del proyecto 2, tecnologías utilizadas, resultados obtenidos.
-""")
-
-# Contacto
-st.header("Contacto")
-st.write("""
-* **Correo electrónico:** andres.vanegas@example.com
-* **LinkedIn:** https://www.linkedin.com/in/andres-vanegas/
-* **GitHub:** https://github.com/andresvanegas
-""")
-
-# Imagen de perfil (opcional)
-if st.checkbox("Mostrar imagen de perfil"):
-    st.image("tu_foto.jpg")
+dfDatos = pd.read_csv(url)
